fb7cbdcb66e706de72324e12c7c55188b1a46f50
k1r0s <ciroreed@gmail.com>
2017-09-08 23:34:04 +0200

    done data structure as a tree

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import axios from "axios";
+import flatMapDeep from "lodash/flatMapDeep";
+import union from "lodash/union";
+import slice from "lodash/slice";
+import map from "lodash/map";
+import filter from "lodash/filter";
+import includes from "lodash/includes";
+import sortBy from "lodash/sortBy";
+import reverse from "lodash/reverse";
 
 axios.get("data/host-app-data.json").then(({ data }) => {
-  console.log(data);
+
+  // limit 100
+  // data = slice(data, 0, 100);
+
+  const hostTree = map(union(flatMapDeep(data, (item => item.host))), (host, index) => {
+    return {
+      host,
+      top5: slice(reverse(sortBy(filter(data, (item) => includes(item.host, host)), 'apdex')), 0, 5)
+    }
+  })
+
+  console.log(hostTree);
+
 });
